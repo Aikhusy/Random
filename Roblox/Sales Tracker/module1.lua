4d613422f014189c23f7dc51e736d3090dc3a0ec
Syaikhul Shafwan R <heymepoo@gmail.com>
2023-07-31 19:01:59 +0700

    smoll commit

---
+++ Roblox/Sales Tracker/module1.lua
@@ -0,0 +1,18 @@
+local module = {}
+
+function module.getInput()
+	--get from gui
+	local SG= game:GetService("StarterGui")
+	local input= SG.InputGUI.InputId
+	
+	
+	local function process()
+		local text=input.Text
+		
+	end
+
+	
+	--process
+	--output
+end
+return module

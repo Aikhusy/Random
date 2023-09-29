c4f198bedb2084c4df31fc9099885f2af0aed69e
Syaikhul Shafwan R <heymepoo@gmail.com>
2023-09-29 17:49:42 +0700

    error

--- Roblox/pointsLeaderStats.lua
+++ Roblox/pointsLeaderStats.lua
@@ -3,6 +3,7 @@ game.Players.PlayerAdded:Connect(function(player)
 	local status=Instance.new("Folder",player)
 	status.Name="leaderstats"
 	
+
 	local Gold=Instance.new("IntValue",status)
 	Gold.Name="Pembelian"
 	
